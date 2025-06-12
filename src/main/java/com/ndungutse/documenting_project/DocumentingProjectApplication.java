7bd2a2febcf7be2369507356d7f87bc78466356a
ericndungutse <dav.ndungutse@gmail.com>
2025-06-12 15:48:17 +0200

    Innitial commit

---
+++ src/main/java/com/ndungutse/documenting_project/DocumentingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ndungutse.documenting_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndungutse/documenting_project/DocumentingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndungutse.documenting_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
