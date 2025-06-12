b7e7e4181df4f0818d9a04432b0b902ea265ff01
ericndungutse <dav.ndungutse@gmail.com>
2025-06-12 16:36:33 +0200

    fix: fix a login bug. change users.getPassowrd to user.getPassowd

--- src/main/java/com/ndungutse/documenting_project/UserController.java
+++ src/main/java/com/ndungutse/documenting_project/UserController.java
@@ -37,7 +37,7 @@ public class UserController {
         String password = loginRequest.getPassword();
 
         for (UserModel user : users) {
-            if (user.getUsername().equals(username) && users.getPassword().equals(password)) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
                 // Create a copy of the user without the password for security
                 UserModel authenticatedUser = new UserModel(user.getUsername(), "[PROTECTED]");
                 return ResponseEntity.ok(authenticatedUser);
